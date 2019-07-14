54c8c1f0357012b4710e6a442272fa4af64044c6
doroot <doroot@192.168.0.27>
2019-07-14 16:14:34 +0530

    changes for parts and accesorry to use brand mentioned in ticket.

--- app/src/main/java/com/ayushya/spring/controller/InventoryAccesoryController.java
+++ app/src/main/java/com/ayushya/spring/controller/InventoryAccesoryController.java
@@ -2,6 +2,7 @@ package com.ayushya.spring.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ayushya.spring.bean.InventoryAccessories;
 import com.ayushya.spring.bean.InventoryParts;
+import com.ayushya.spring.bean.Tickets;
 import com.ayushya.spring.repository.InventoryAccessoryRepository;
 import com.ayushya.spring.repository.InventoryPartsRepository;
+import com.ayushya.spring.repository.TicketsRepository;
 import com.ayushya.spring.service.InventoryService;
 import com.ayushya.spring.service.NextSequenceService;
 
@@ -30,22 +33,39 @@ public class InventoryAccesoryController {
 	@Autowired
 	private InventoryAccessoryRepository repository;
 	
+	@Autowired
+	private TicketsRepository ticketsRepository;
 	
 	@Autowired
 	private NextSequenceService nextSequenceService;
 	
 	@RequestMapping(value = "/", method = RequestMethod.POST)
-	public InventoryAccessories createNewItem(@Valid @RequestBody InventoryAccessories itemDetails) {
+	public InventoryAccessories createNewItem(@Valid @RequestBody InventoryAccessories itemDetails,HttpServletRequest request) {
 		itemDetails.set_id(nextSequenceService.getNextSequence("customSequences_accessory"));
+		String ticket_id = request.getHeader("x-ticket");
+		if(ticket_id!=null) {
+			Tickets t = ticketsRepository.findOne(ticket_id);
+			itemDetails.setModel(t.getModel_name());
+			itemDetails.setBrand(t.getBrand());
+			itemDetails.setCategory(t.getProduct_category());
+			itemDetails.setSubCategory(t.getProduct_sub_category());
+		}
 		itemDetails.setStatus("Available");
 		return repository.save(itemDetails);
 	}
 
 	
 	@RequestMapping(value = "/request", method = RequestMethod.POST)
-	public InventoryAccessories requestNewAccessory(@RequestParam("type") String requestType,@Valid @RequestBody InventoryAccessories itemDetails) {
+	public InventoryAccessories requestNewAccessory(HttpServletRequest request,@RequestParam("type") String requestType,@Valid @RequestBody InventoryAccessories itemDetails) {
 		itemDetails.set_id(nextSequenceService.getNextSequence("customSequences_accessory"));
-		itemDetails.setStatus("Available");
+		String ticket_id = request.getHeader("x-ticket");
+		if(ticket_id!=null) {
+			Tickets t = ticketsRepository.findOne(ticket_id);
+			itemDetails.setModel(t.getModel_name());
+			itemDetails.setBrand(t.getBrand());
+			itemDetails.setCategory(t.getProduct_category());
+			itemDetails.setSubCategory(t.getProduct_sub_category());
+		}		itemDetails.setStatus("Available");
 		if(requestType!=null && requestType.contentEquals("true"))	itemDetails.setStatus("Requested");
 		return inventoryService.requestNewAccessory(itemDetails);
 	}
@@ -65,9 +85,25 @@ public class InventoryAccesoryController {
 		return inventoryService.updateAccessory(accessoryId, itemDetails);
 	}
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public List<InventoryAccessories> getAllAccessories(){
-		return repository.findAll();
-	}
+//	@RequestMapping(value = "/", method = RequestMethod.GET)
+//	public List<InventoryAccessories> getAllAccessories(){
+//		return repository.findAll();
+//	}
 
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public Iterable<InventoryAccessories> getPartsBelongingToTicket(HttpServletRequest request){
+		String ticket_id = request.getHeader("x-ticket");
+		if(ticket_id!=null) {
+			Tickets t = ticketsRepository.findOne(ticket_id);
+			String model = t.getModel_name();
+			String brand = t.getBrand();
+			String category = t.getProduct_category();
+			String subCategory = t.getProduct_sub_category();
+			if(subCategory!=null)
+			return inventoryService.fetchMatchingAccessory(brand,category,subCategory,model);
+			else return inventoryService.fetchMatchingAccessoryWithoutSubCategory(brand, category, model);
+
+		}
+		return null;
+	}
 }
--- app/src/main/java/com/ayushya/spring/controller/InventoryPartsController.java
+++ app/src/main/java/com/ayushya/spring/controller/InventoryPartsController.java
@@ -1,6 +1,7 @@
 package com.ayushya.spring.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ayushya.spring.bean.InventoryAccessories;
 import com.ayushya.spring.bean.InventoryParts;
+import com.ayushya.spring.bean.Tickets;
 import com.ayushya.spring.repository.InventoryAccessoryRepository;
 import com.ayushya.spring.repository.InventoryPartsRepository;
+import com.ayushya.spring.repository.TicketsRepository;
 import com.ayushya.spring.service.InventoryService;
 import com.ayushya.spring.service.NextSequenceService;
 
@@ -29,20 +32,39 @@ public class InventoryPartsController {
 	@Autowired
 	private InventoryPartsRepository repository;
 	
+	@Autowired
+	private TicketsRepository ticketsRepository;
+	
 	
 	@Autowired
 	private NextSequenceService nextSequenceService;
 	
 	@RequestMapping(value = "/", method = RequestMethod.POST)
-	public InventoryParts createNewItem(@Valid @RequestBody InventoryParts inventoryParts) {
+	public InventoryParts createNewItem(@Valid @RequestBody InventoryParts inventoryParts,HttpServletRequest request, HttpServletResponse response) {
 		inventoryParts.set_id(nextSequenceService.getNextSequence("customSequences_parts"));
+		String ticket_id = request.getHeader("x-ticket");
+		if(ticket_id!=null) {
+			Tickets t = ticketsRepository.findOne(ticket_id);
+			inventoryParts.setModel(t.getModel_name());
+			inventoryParts.setBrand(t.getBrand());
+			inventoryParts.setCategory(t.getProduct_category());
+			inventoryParts.setSubCategory(t.getProduct_sub_category());
+		}
 		inventoryParts.setStatus("Available");
 		return repository.save(inventoryParts);
 	}
 	
 	@RequestMapping(value = "/request", method = RequestMethod.POST)
-	public InventoryParts requestNewItem(@Valid @RequestBody InventoryParts inventoryParts,@RequestParam("type") String requestType,HttpServletRequest httpServletRequest) {
+	public InventoryParts requestNewItem(HttpServletRequest request,@Valid @RequestBody InventoryParts inventoryParts,@RequestParam("type") String requestType,HttpServletRequest httpServletRequest) {
 		inventoryParts.set_id(nextSequenceService.getNextSequence("customSequences_parts"));
+		String ticket_id = request.getHeader("x-ticket");
+		if(ticket_id!=null) {
+			Tickets t = ticketsRepository.findOne(ticket_id);
+			inventoryParts.setModel(t.getModel_name());
+			inventoryParts.setBrand(t.getBrand());
+			inventoryParts.setCategory(t.getProduct_category());
+			inventoryParts.setSubCategory(t.getProduct_sub_category());
+		}
 		inventoryParts.setStatus("Available");
 		if(requestType!=null && requestType.contentEquals("true"))inventoryParts.setStatus("Requested");
 		return inventoryPartService.requestNewPart(inventoryParts);
@@ -66,9 +88,28 @@ public class InventoryPartsController {
 		return inventoryPartService.updatePart(partId, itemDetails);
 	}
 	
+//	@RequestMapping(value = "/", method = RequestMethod.GET)
+//	public Iterable<InventoryParts> getAllItems(){
+//		return repository.findAll();
+//	}
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public Iterable<InventoryParts> getAllItems(){
-		return repository.findAll();
+	public Iterable<InventoryParts> getPartsBelongingToTicket(HttpServletRequest request){
+		String ticket_id = request.getHeader("x-ticket");
+		if(ticket_id!=null) {
+			Tickets t = ticketsRepository.findOne(ticket_id);
+			String model = t.getModel_name();
+			String brand = t.getBrand();
+			String category = t.getProduct_category();
+			String subCategory = t.getProduct_sub_category();
+			if(subCategory!=null)
+			return inventoryPartService.fetchMatchingParts(brand,category,subCategory,model);
+			else return inventoryPartService.fetchMatchingPartsWithoutSubCategory(brand, category, model);
+
+		}
+		return null;
 	}
+	
+	
 
 }
